80055b4a8c279ec0c287aa89e5f2ad1d86e08224
James Xu <xumingming64398966@gmail.com>
2012-12-30 21:08:57 +0800

    reorgnize the function order

--- src/dubbo/core.clj
+++ src/dubbo/core.clj
@@ -6,87 +6,17 @@
 
 (def ^{} registry-atom (atom nil))
 (def ^{} services-atom (atom {}))
-(declare list-services)
 
-(defn create-registry
-  "Creates a dubbo registry with the specified address."
-  [address]
-  (doto (RegistryConfig.)
-    (.setAddress address)))
+(declare list-services create-service-reference0 clojurify javarify
+         get-service close-service close-all-services
+         create-registry create-service-reference
+         service-exists? create-service-if-needed
+         add-service-method)
 
 (defn set-registry!
   "Sets the dubbo registry"
   [address]
   (reset! registry-atom (create-registry address)))
-
-(defn create-service-reference0
-  "Creates a service instance for the specified service name."
-  [registry service-name]
-  (let [reference (doto (ReferenceConfig.)
-                    (.setApplication (ApplicationConfig. "dubbo.clj"))
-                    (.setInterface service-name)
-                    (.setGeneric true)
-                    (.setRegistry registry))]
-    reference))
-
-(defn create-service-reference
-  [service-name]
-  (create-service-reference0 @registry-atom service-name))
-
-(defn service-exists? [service-name]
-  (contains? @services-atom service-name))
-
-(defn create-service-if-needed [service-name]
-  (when (not (service-exists? service-name))
-    (let [reference (create-service-reference service-name)
-          service-info {:name service-name
-                        :obj reference
-                        :methods {}}]
-      ;; put service into cache
-      (swap! services-atom assoc-in [service-name] service-info))))
-
-(defn get-service
-  "Gets a service instance for the specified full-quantified service name"
-  [service-name]
-    (.get (get-in @services-atom [service-name :obj])))
-
-(defn close-service [service-name]
-  (let [service-reference (get-in @services-atom [service-name :obj])]
-    ;; close the service reference
-    (.destroy service-reference)
-    ;; remove it from the service-atom
-    (swap! services-atom dissoc service-name)))
-
-(defn close-all-services []
-  (let [all-service-names (list-services)]
-    (doseq [service-name all-service-names]
-      (close-service service-name))))
-
-(defn clojurify [obj]
-  ;; if its a map, we prettify it:
-  ;; 1) string key -> keyword key
-  ;; 2) HashMap -> clojure map
-  (if (instance? HashMap obj)
-    (let [ret (map (fn [[k v]] [(keyword k) v]) obj)
-          ret (into {} ret)]
-      ret)
-    obj))
-
-(defn javarify [param]
-  (if (map? param)
-    (let [ret (map (fn [[k v]] [(name k) v]) param)
-          java-map (HashMap.)]
-      (doseq [[k v] ret]
-        (.put java-map k v))
-      java-map)
-    param))
-
-(defn add-service-method
-  [services-atom service-name method-name param-types]
-    ;; add the method information into the services-atom
-  (swap! services-atom assoc-in
-         [service-name :methods method-name]
-         {:param-types param-types}))
 
 (defmacro def-service-method
   "Defines a remote service method as local function in current namespace.
@@ -136,4 +66,80 @@
   "Lists all the methods of a dubbo services we defined."
   [service-name]
   (keys (get-in @services-atom [service-name :methods])))
+
+(defn create-service-reference0
+  "Creates a service instance for the specified service name."
+  [registry service-name]
+  (let [reference (doto (ReferenceConfig.)
+                    (.setApplication (ApplicationConfig. "dubbo.clj"))
+                    (.setInterface service-name)
+                    (.setGeneric true)
+                    (.setRegistry registry))]
+    reference))
+
+(defn create-service-reference
+  "Creates a dubbo service reference."
+  [service-name]
+  (create-service-reference0 @registry-atom service-name))
+
+(defn clojurify [obj]
+  ;; if its a map, we prettify it:
+  ;; 1) string key -> keyword key
+  ;; 2) HashMap -> clojure map
+  (if (instance? HashMap obj)
+    (let [ret (map (fn [[k v]] [(keyword k) v]) obj)
+          ret (into {} ret)]
+      ret)
+    obj))
+
+(defn javarify [param]
+  (if (map? param)
+    (let [ret (map (fn [[k v]] [(name k) v]) param)
+          java-map (HashMap.)]
+      (doseq [[k v] ret]
+        (.put java-map k v))
+      java-map)
+    param))
+
+(defn add-service-method
+  [services-atom service-name method-name param-types]
+    ;; add the method information into the services-atom
+  (swap! services-atom assoc-in
+         [service-name :methods method-name]
+         {:param-types param-types}))
+
+(defn get-service
+  "Gets a service instance for the specified full-quantified service name"
+  [service-name]
+    (.get (get-in @services-atom [service-name :obj])))
+
+(defn close-service [service-name]
+  (let [service-reference (get-in @services-atom [service-name :obj])]
+    ;; close the service reference
+    (.destroy service-reference)
+    ;; remove it from the service-atom
+    (swap! services-atom dissoc service-name)))
+
+(defn close-all-services []
+  (let [all-service-names (list-services)]
+    (doseq [service-name all-service-names]
+      (close-service service-name))))
+
+(defn create-registry
+  "Creates a dubbo registry with the specified address."
+  [address]
+  (doto (RegistryConfig.)
+    (.setAddress address)))
+
+(defn service-exists? [service-name]
+  (contains? @services-atom service-name))
+
+(defn create-service-if-needed [service-name]
+  (when (not (service-exists? service-name))
+    (let [reference (create-service-reference service-name)
+          service-info {:name service-name
+                        :obj reference
+                        :methods {}}]
+      ;; put service into cache
+      (swap! services-atom assoc-in [service-name] service-info))))
 
